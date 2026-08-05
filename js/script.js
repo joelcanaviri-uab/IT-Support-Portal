c27330d2d81cf6455291f66fb25f0a2e86e095af
joelcanaviri-uab <joel.canaviri@uab.edu.bo>
2026-08-04 21:43:23 -0400

    actualizacion script 4.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ const navLinks = document.querySelector(".nav-links");
 menuButton.addEventListener("click", function () {
     navLinks.classList.toggle("active");
 });
+
+const formulario = document.querySelector("#form-contacto");
+const aviso = document.querySelector("#aviso-contacto");
+
+function revisarFormulario(event) {
+
+    event.preventDefault();
+
+    const nombre = document.querySelector("#name").value;
+    const correo = document.querySelector("#email").value;
+    const mensaje = document.querySelector("#message").value;
+    if (nombre === "") {
+        aviso.textContent = "Por favor ingrese su nombre.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    }
+    else if (!correo.includes("@")) {
+        aviso.textContent = "Ingrese un correo electrónico válido.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    }
+    else if (mensaje === "") {
+        aviso.textContent = "Por favor escriba su mensaje.";
+        aviso.classList.add("error");
+        aviso.classList.remove("exito");
+    }
+    else {
+        aviso.textContent =
+        "✔ Gracias por contactarnos. Su solicitud fue enviada correctamente.";
+        aviso.classList.add("exito");
+        aviso.classList.remove("error");
+        formulario.reset();
+    }
+}
+formulario.addEventListener("submit", revisarFormulario);
